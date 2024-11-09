84fe0db0cf2d5cb78bc0269043a02aa2f51251a0
XJEG <12123195+xjeg@user.noreply.gitee.com>
2024-11-09 12:16:01 +0800

    origin

---
+++ src/main/java/com/taotu/hv/TaotuApplication.java
@@ -0,0 +1,13 @@
+package com.taotu.hv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaotuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaotuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taotu/hv/TaotuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taotu.hv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaotuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
